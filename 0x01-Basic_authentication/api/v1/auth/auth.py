8fa58128586e645a53db92fa9ac4486bedd72be2
erdey <erdeysyoum@gmail.com>
2024-08-06 12:32:55 +0000

    authentication initial

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Basic authentication module """
+from flask import request
+
+
+class Auth:
+    """Auth class """
+
+    def __init__(self):
+        """ Initializer """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ """
+        pass
+    def authorization_header(self, request=None) -> str:
+        """ """
+        pass
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ """
+        pass
