08ddfe08a85bb779e47413dbdb535c853fb4ea50
erdey <erdeysyoum@gmail.com>
2024-08-07 07:16:04 +0000

    authentication basic

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,13 @@ class Auth:
     """Auth class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+        """ add authentication """
         pass
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """ add authorization """
         pass
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """ current user """
         pass
