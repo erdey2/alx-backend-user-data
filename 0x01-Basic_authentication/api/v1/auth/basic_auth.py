e9c83290badb56dd0e4db47c1877ef03144bae2c
erdey <erdeysyoum@gmail.com>
2024-08-07 12:48:34 +0000

    retrieve user instance

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -62,7 +62,7 @@ class BasicAuth(Auth):
 
         return credentials[0], credentials[1]
 
-     def user_object_from_credentials(self, user_email: str,
+    def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """ Returns the User instance """
         if user_email is None or not isinstance(user_email, str):
@@ -81,3 +81,29 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+
+        return user
