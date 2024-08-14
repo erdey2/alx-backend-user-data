4b966e59c95218f4129eb89d687da34eada8b5f3
erdey <erdeysyoum@gmail.com>
2024-08-14 08:53:22 +0000

    basemodel defined

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,9 +10,9 @@ Base = declarative_base()
 class User(Base):
     """ Base model """
     __tablename__ = "users"
-    
-    id = Column(integer, primary_key=True),
-    email = Column(String(250), nullable=False),
-    hash_password = Column(String(250), nullable=False),
-    session_id = Column(String(250)),
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hash_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
     reset_token = Column(string(250))
