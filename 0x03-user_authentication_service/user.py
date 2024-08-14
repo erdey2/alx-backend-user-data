db35de88090ea79f80a576aaf9415f0fb3ca3b3c
erdey <erdeysyoum@gmail.com>
2024-08-14 08:45:33 +0000

    basemodel defined

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,6 +2,9 @@
 
 """ Defining a model module """
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class User(Base):
